cb2a645a5580be80628b54598eae63fd384b0650
Kinoshita.Yasuhiro <WhoIsDissolvedGirl+github@gmail.com>
2016-09-26 01:04:50 +0900

    Change Esc to F8

--- keyboards/ergodox/keymaps/kinoppyd/keymap.c
+++ keyboards/ergodox/keymaps/kinoppyd/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   | Ctrl |  Cmd | Cmd  |  Alt |  Alt |                                       |  Alt |   -  |  [   |   ]  |  Alt |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | Esc  | Esc  |       | Left | Right  |
+ *                                        | Esc  |  F8  |       | Left | Right  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Esc  |       |  Up  |        |      |
  *                                 | Space|Backsp|------|       |------|  Esc   |Enter |
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
         KC_LCTL,        KC_LGUI,      KC_LGUI,KC_LALT,KC_LALT,
-                                              KC_ESC,  KC_ESC,
+                                              KC_ESC,  KC_F8,
                                                               KC_ESC,
                                                KC_SPC,KC_BSPC,KC_ESC,
         // right hand
