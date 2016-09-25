b65878d59335852922035908835906d45267cf32
Kinoshita.Yasuhiro <WhoIsDissolvedGirl+github@gmail.com>
2016-09-26 01:05:06 +0900

    Delete Right Enter and move Quot and Slash

--- keyboards/ergodox/keymaps/kinoppyd/keymap.c
+++ keyboards/ergodox/keymaps/kinoppyd/keymap.c
@@ -15,9 +15,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   | Enter  |
+ * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   '"   |
  * |--------+------+------+------+------+------| Hyper|           | Tab  |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  '"  |   /    |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   |   /    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | Ctrl |  Cmd | Cmd  |  Alt |  Alt |                                       |  Alt |   -  |  [   |   ]  |  Alt |
  *   `----------------------------------'                                       `----------------------------------'
@@ -44,8 +44,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
              KC_F10,      KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_EQL,
              TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_ENT,
-             KC_TAB,      KC_N,   KC_M,   KC_COMM,KC_DOT, KC_QUOT,          KC_SLSH,
+                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
+             KC_TAB,      KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_SLSH,
                                   KC_LALT,KC_MINS,KC_LBRC,KC_RBRC,          KC_LALT,
              KC_LEFT,        KC_RIGHT,
              KC_UP,
