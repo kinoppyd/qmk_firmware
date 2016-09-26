c2bd92b0103bdea6022c9711e27dc6d8df8a04d3
Kinoshita.Yasuhiro <WhoIsDissolvedGirl+github@gmail.com>
2016-09-26 20:57:34 +0900

    Change left alt to BS

--- keyboards/ergodox/keymaps/kinoppyd/keymap.c
+++ keyboards/ergodox/keymaps/kinoppyd/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Tab  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   |   /    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Ctrl |  Cmd | Cmd  |  Alt |  Alt |                                       |   -  |   -  |  [   |   ]  |  Alt |
+ *   | Ctrl |  Cmd | Cmd  |  Alt |  BS  |                                       |   -  |   -  |  [   |   ]  |  Alt |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Esc  |  F8  |       | Left | Right  |
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        KC_LCTL,        KC_LGUI,      KC_LGUI,KC_LALT,KC_LALT,
+        KC_LCTL,        KC_LGUI,      KC_LGUI,KC_LALT,KC_BSPC,
                                               KC_ESC,  KC_F8,
                                                               KC_ESC,
                                                KC_SPC,KC_SPC, KC_BSPC,
