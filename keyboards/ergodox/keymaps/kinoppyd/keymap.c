b13cf02650423ad5b2c4f0a28b7ac5620b869661
Kinoshita.Yasuhiro <WhoIsDissolvedGirl+github@gmail.com>
2017-01-17 15:23:26 +0900

    Move F8 key

--- keyboards/ergodox/keymaps/kinoppyd/keymap.c
+++ keyboards/ergodox/keymaps/kinoppyd/keymap.c
@@ -22,9 +22,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   | Ctrl |  Cmd | Cmd  |  Alt |  BS  |                                       |  L2  |      |  [   |   ]  |  Alt |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      |  F8  |       |      |        |
+ *                                        |      |      |       |  F10 |        |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      |      |       |  F10 |        |      |
+ *                                 |      |      |      |       |  F8  |        |      |
  *                                 | Space|Space |------|       |------|  Esc   |Enter |
  *                                 |      |      |      |       |  F7  |        |      |
  *                                 `--------------------'       `----------------------'
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   MO(MDIA),
         KC_LCTL,        KC_LGUI,      KC_LGUI,KC_LALT,KC_BSPC,
-                                               KC_NO,  KC_F8,
+                                               KC_NO,  KC_NO,
                                                                KC_NO,
                                                KC_SPC, KC_SPC, KC_NO,
         // right hand
@@ -47,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_H,   KC_J,    KC_K,   KC_L,   KC_SCLN,  KC_QUOT,
              KC_TAB,      KC_N,   KC_M,    KC_COMM,KC_DOT, KC_SLSH,  KC_SLSH,
                                   MO(MDIA),KC_NO  ,KC_LBRC,KC_RBRC,  KC_LALT,
-             KC_NO,          KC_NO,
-             KC_F10,
+             KC_F10,          KC_NO,
+             KC_F8,
              KC_F7,  KC_ESC, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
