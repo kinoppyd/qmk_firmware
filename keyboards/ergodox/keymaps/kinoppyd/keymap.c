80abe288c403b6ee520652c1223b5048bca9ad53
kinoppyd <WhoIsDissolvedGirl+github@gmail.com>
2017-03-10 17:01:27 +0900

    Move btn keys

--- keyboards/ergodox/keymaps/kinoppyd/keymap.c
+++ keyboards/ergodox/keymaps/kinoppyd/keymap.c
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |Brwser|
- *                                 |      |      |------|       |------|      |Back  |
+ *                                 | Lclk | Rclk |------|       |------|      |Back  |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -123,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_TRNS, KC_TRNS, KC_LALT,  KC_TRNS,
                                             KC_TRNS, KC_TRNS,
                                                      KC_TRNS,
-                                  KC_TRNS,  KC_TRNS, KC_TRNS,
+                                  KC_BTN1,  KC_BTN2, KC_TRNS,
     // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,
        KC_TRNS,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS, KC_TRNS,
